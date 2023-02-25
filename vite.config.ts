cce6338511746ebd68268108325ac95be673339e
Bartosz <bartoszmrosek@op.pl>
2023-02-25 13:50:45 +0100

    Small config change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: "/<repo>/",
   plugins: [react(), eslint()],
   test: {
     globals: true,
